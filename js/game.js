7fc35a7ce9a206c02f618bee1a60e4ab67371ca7
lindolo25 <lindolo25@outlook.com>
2018-09-16 18:25:19 -0400

    game completed, UI changes for movile done.

--- js/game.js
+++ js/game.js
@@ -5,20 +5,16 @@ var marvelGame = {
     enemiesLeft: 0,
     init: function()
     {
-        // reset the game ----------------------------------------------------------------------------
         this.reset();
         
-        // create the click events ---------------------------------------------------
         $(".charapter").on("click", function(){
             marvelGame.charapterClicked(this);
         });
 
-        // set the attack event ----------------------------------------------------------------------
         $("#attack").on("click", function(){
             marvelGame.attack();
         });
 
-        // set reset event ----------------------------------------------------------------------------
         $("#resetGame").on("click", function(){
             marvelGame.reset();
         });
@@ -29,11 +25,9 @@ var marvelGame = {
         {
             this.main = $(charapter).attr("data-index");
             $(charapter).appendTo("#main-area");
-            console.log(this.charapters[this.main].healthPoints);
 
             $("main").show();
             $("#chose-area").hide();
-            // Move the rest of the charapters to the Enemies Area -------------------------------------
             var toMove = $("#chose-area>.charapter")
             this.enemiesLeft = toMove.length;
             for(i = 0; i < toMove.length; i++)
@@ -45,7 +39,6 @@ var marvelGame = {
         {
             this.defender = $(charapter).attr("data-index");
             $(charapter).appendTo("#defender-area");
-            console.log(this.charapters[this.defender].healthPoints);
         }
         this.log("","");
     },
@@ -55,12 +48,9 @@ var marvelGame = {
         else if(this.defender === undefined) this.log("Choose a defender from the enemies list", "alert-info");
         else
         {
-            console.log("an attack");
-            // main attack defender
             var defenderHP = this.reduceCharapterHP(this.defender,this.charapters[this.main].attackPower);
-            // defender will use the counter attack to reduce main HP
             var mainHP = this.reduceCharapterHP(this.main,this.charapters[this.defender].counterAttackPower);
-            // compute the result
+            
             if(mainHP <= 0)
             {
                 this.log("You lose, Game Over", "alert-danger");
@@ -87,41 +77,35 @@ var marvelGame = {
             }
             else
             {
-                this.log("You have damage X by "+ this.charapters[this.main].attackPower +"<br> y attacked back with a damage of " + this.charapters[this.defender].counterAttackPower, "alert-info");
+                this.log("You have damage "+ this.charapters[this.defender].name +" by "+ this.charapters[this.main].attackPower +" Points<br> "+ this.charapters[this.defender].name +" attacked back with a damage of " + this.charapters[this.defender].counterAttackPower, "alert-info");
             }
             
-            // increase main attack power by base.
             this.charapters[this.main].attackPower += this.charapters[this.main].basePower;
         }      
     },
     reset: function()
     {
-        console.log("Reset");
-        // empty game's properties --------------------------------------------------------------------
         this.charapters = [];
         this.main = undefined;
         this.defender = undefined;
         this.enemiesLeft = 0;
 
-        // load charapters from HTML ------------------------------------------------------------------
         var charaptersTemp = $(".charapter");
         for(i = 0; i < charaptersTemp.length; i++)
         {
             this.charapters.push({
                 ID: $(charaptersTemp[i]).attr("id"),
+                name: $(charaptersTemp[i]).attr("data-name"),
                 healthPoints: parseInt($(charaptersTemp[i]).attr("data-health-points")),
                 attackPower: parseInt($(charaptersTemp[i]).attr("data-attack-power")),
                 counterAttackPower : parseInt($(charaptersTemp[i]).attr("data-counter-attack-power")),
                 basePower : parseInt($(charaptersTemp[i]).attr("data-attack-power"))
             });
             $(charaptersTemp[i]).attr("data-index", i);
-            $("#" + $(charaptersTemp[i]).attr("id") +">#health-points").text($(charaptersTemp[i]).attr("data-health-points"));
-
-            // append all charapters to the Choose-Area -----------------------------------------------
+            $("#" + $(charaptersTemp[i]).attr("id") +" #health-points").text($(charaptersTemp[i]).attr("data-health-points"));
             $(charaptersTemp[i]).appendTo("#chose-area");
         }
         
-        // show and hide the sections -----------------------------------------------------------------
         $("main").hide();
         $("#resetGame").hide();
         $('#chose-area').show();
@@ -135,7 +119,7 @@ var marvelGame = {
     {
         charapter = this.charapters[charapter];
         charapter.healthPoints -= points;
-        $("#" + charapter.ID +">#health-points").text(charapter.healthPoints);
+        $("#" + charapter.ID +" #health-points").text(charapter.healthPoints);
         return charapter.healthPoints;
     }
 
